c875526e36c4763741834b2a2750c92bc5f97b70
Pongsapat1035 <pongsapat357@gmail.com>
2025-01-11 13:02:51 +0700

    fix : import daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import daisyui from 'daisyui'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -15,6 +16,6 @@ export default {
     themes: [],
   },
   plugins: [
-    require('daisyui'),
+    daisyui
   ],
 }
